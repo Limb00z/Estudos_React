b00f479bcf515512500590b98a83edc21c967643
Ruan Reis <ruancosme23@gmail.com>
2023-03-13 12:32:20 -0300

    finalização sobre importação de imagens

--- 3_inicio/iniciar/src/App.js
+++ 3_inicio/iniciar/src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
 
+import p2 from "./assets/p2.jpg";
+
 function App() {
   return (
     <div className="App">
-      
+      <h1>Avançando em React</h1>
+      <div ><img className="Porta" src="/p1.jpg" alt="uma porta" /></div>
+      <div ><img className="Porta" src={p2} alt="Uma porta" /></div>
     </div>
   );
 }

44a44d33336b0c22a36213d470dd883f85933a9f
Ruan Reis <ruancosme23@gmail.com>
2023-03-13 12:36:55 -0300

    comentários

--- 3_inicio/iniciar/src/App.js
+++ 3_inicio/iniciar/src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
 
-import p2 from "./assets/p2.jpg";
+import p2 from "./assets/p2.jpg"; /*importando uma imagem da pasta assets, para usar*/
 
 function App() {
   return (
     <div className="App">
       <h1>Avançando em React</h1>
-      <div ><img className="Porta" src="/p1.jpg" alt="uma porta" /></div>
-      <div ><img className="Porta" src={p2} alt="Uma porta" /></div>
+      <div ><img className="Porta" src="/p1.jpg" alt="uma porta" /></div> {/*Importando via pasta public*/}
+      <div ><img className="Porta" src={p2} alt="Uma porta" /></div> {/*Importando via pasta assets, precisa ser importada, no inicio.*/}
     </div>
   );
 }

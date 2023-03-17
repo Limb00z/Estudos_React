4caade0fefbc1bcdbca93a7637589b16599b7f93
Ruan Reis <ruancosme23@gmail.com>
2023-03-17 00:02:25 -0300

    entendimento sobre img src e public

--- 4_inicio-2/testeVite/vite-project/src/App.jsx
+++ 4_inicio-2/testeVite/vite-project/src/App.jsx
@@ -11,12 +11,13 @@ function App() {
         <h1>Aprendendo sobre:</h1>
       </div>
       <h2>Vite + React</h2>
-      <img src={Comparacao} className="foto"/>
+      <img src={Comparacao} className="foto"/>  {/*Para exibir essa imagem que está no src, precisa importar*/}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           contador: {count}
         </button>
-       </div>
+      </div>
+      <img src="/img/3x4-png.png"/> {/*Como está no Public, não precisa importar no início*/}
     </div>
   )
 }
